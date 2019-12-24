9bbfd3ece1e3e686c2898e053c411cfc1c6a706a
thoriqdharmawan <thoriqd21@gmail.com>
2019-12-24 17:03:19 +0700

    is the backend complete, sir ?

--- functions/index.js
+++ functions/index.js
@@ -39,12 +39,49 @@ app.get('/screams', (req, res) => {
             return res.json(screams);
         })
         .catch(err => console.log(err));
-})
+});
 
-app.post('/scream', (req, res) => {
+const FBAuth = (req, res, next) => {
+    let idToken;
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+        idToken = req.headers.authorization.split('Bearer ')[1];
+    } else {
+        console.error('No token found')
+        return res.status(403).json({
+            error: 'Unauthorized'
+        })
+    }
+
+    admin.auth().verifyIdToken(idToken)
+        .then(decodedToken => {
+            req.user = decodedToken;
+            console.log(decodedToken);
+            return db.collection('users')
+                .where('userId', '==', req.user.uid)
+                .limit(1)
+                .get();
+        })
+        .then(data => {
+            req.user.handle = data.docs[0].data().handle;
+            return next();
+        })
+        .catch(err => {
+            console.error('Error while verifying token', err);
+            return res.status(403).json(err);
+        })
+}
+
+// Post scream
+app.post('/scream', FBAuth, (req, res) => {
+    if (req.body.body.trim() === '') {
+        return res.status(400).json({
+            body: 'Body must not be empty'
+        });
+    }
+
     const newScream = {
         body: req.body.body,
-        userHandle: req.body.userHandle,
+        userHandle: req.user.handle,
         createdAt: new Date().toISOString()
     };
 
@@ -64,6 +101,18 @@ app.post('/scream', (req, res) => {
         });
 });
 
+const isEmail = (email) => {
+    const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (email.match(regEx)) return true;
+    else return false;
+}
+
+const isEmpty = (string) => {
+    if (string.trim() === '') return true;
+    else return false;
+}
+
 
 // signup route
 app.post('/signup', (req, res) => {
@@ -73,6 +122,20 @@ app.post('/signup', (req, res) => {
         confirmPassword: req.body.confirmPassword,
         handle: req.body.handle,
     };
+
+    let errors = {};
+
+    if (isEmpty(newUser.email)) {
+        errors.email = 'Email must not be empty'
+    } else if (!isEmail(newUser.email)) {
+        errors.email = 'Must be a valid email address'
+    }
+
+    if (isEmpty(newUser.password)) errors.password = 'Must not be empty';
+    if (newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'Passwords must match';
+    if (isEmpty(newUser.handle)) errors.handle = 'Must not be empty';
+
+    if (Object.keys(errors).length > 0) return res.status(400).json(errors);
 
     // TODO validate data
     let token, userId;
@@ -92,8 +155,8 @@ app.post('/signup', (req, res) => {
             userId = data.user.uid;
             return data.user.getIdToken();
         })
-        .then(token => {
-            token = token;
+        .then(idToken => {
+            token = idToken;
             const userCredentials = {
                 handle: newUser.handle,
                 email: newUser.email,
@@ -119,6 +182,41 @@ app.post('/signup', (req, res) => {
                 });
             }
 
+        });
+});
+
+app.post('/login', (req, res) => {
+    const user = {
+        email: req.body.email,
+        password: req.body.password,
+    }
+
+    let errors = {};
+
+    if (isEmpty(user.email)) errors.email = 'Must not be empty';
+    if (isEmpty(user.password)) errors.password = 'Must not be empty';
+
+    if (Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+    firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+        .then(data => {
+            return data.user.getIdToken();
+        })
+        .then(token => {
+            return res.json({
+                token
+            });
+        })
+        .catch(err => {
+            if (err.code === 'auth/wrong-password') {
+                return res.status(403).json({
+                    general: 'Wrong credentials, please try again'
+                });
+            } else {
+                return res.status(500).json({
+                    error: err.code
+                });
+            }
         });
 });
 
